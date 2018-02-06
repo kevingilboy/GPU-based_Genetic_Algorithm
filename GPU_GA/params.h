c57330c0893d416d8abc28458b7698f589c3d757
Kevin Gilboy <kmg130@pitt.edu>
2018-02-06 16:35:50 -0500

    Added tcell include to here
    This is the only file the user should touch

--- GPU_GA/params.h
+++ GPU_GA/params.h
@@ -3,14 +3,21 @@
  * Constants and macros used across the project
  */
 
-/**
+/*
+ * PROTEINS AND RULES FILE
+ */
+#include "tcell.c"
+
+/*
  * SIMULATOR CONSTANTS
  */
 #define STEPS 1500
 #define TRIALS 200
 
-/**
+/*
  * GA CONSTANTS
  */
 #define INIT_POPULATION_SIZE 300
 #define INDIVIDUALS_TO_KEEP 20
+#define P_MUTATE 0.2
+#define P_MATE 0.3
