a11c16752cd2e651a4ded0c2bc1241f14540d655
Kevin Gilboy <kmg130@pitt.edu>
2018-02-10 00:07:36 -0500

    Fixed logic of the simulator to track cumulative state

--- GPU_GA/simulator.c
+++ GPU_GA/simulator.c
@@ -8,11 +8,13 @@
 
 int simulate(char *state) {
 	double error = 0;
+	double cumulative_state[FULL_SIZE];
 
 	for (int init_condition_num = 0; init_condition_num < NUM_INITIAL_CONDITIONS; init_condition_num++) {
 		initialize_state(state, init_condition_num);
 
 		for (int trial = 0; trial < TRIALS; trial++) {
+			//Run the trial
 			for (int step = 0; step < STEPS; step++) {
 				//Generate rule
 				int r;
@@ -22,8 +24,20 @@ int simulate(char *state) {
 
 				run_rule(state, r);
 			}
-			error += calculate_error(state);
+			//Trial finished, add to cumulative state for all trials
+			for (int i = 0; i < FULL_SIZE; i++) {
+				//Should I check if state is -1 here? Shouldnt matter...
+				cumulative_state[i] += state[i];
+			}
 		}
+
+		//Calculate average of each protein across all trials
+		for (int i = 0; i < FULL_SIZE; i++) {
+			cumulative_state[i] /= TRIALS;
+		}
+
+		//Calculate error
+		error += calculate_error(cumulative_state, init_condition_num);
 	}
 
 	int ceil_error = ceil(error);
